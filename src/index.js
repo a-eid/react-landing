0c7c0d5ddb42b109a38b203d198d525677ecb589
Ahmed Eid <a.eid@yandex.com>
2017-09-19 10:25:28 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
-
-import "./index.css";
-import App from "./components/app/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
